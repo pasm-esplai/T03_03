106cd6605540300643ab185f9566c04aa1aa0f3b
Usuario <Usuario@LAPTOP-M2PQ13GV>
2021-06-17 20:27:33 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace T03_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+                int X = 14;
+                int Y = 7;
+                double N = 50.25;
+                double M = -4.786;
+
+                Console.WriteLine("X = " + X);
+                Console.WriteLine("Y = " + Y);
+                Console.WriteLine("N = " + N);
+                Console.WriteLine("M = " + M);
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("X + Y = " + (X + Y));
+                Console.WriteLine("X - Y = " + (X - Y));
+                Console.WriteLine("X * Y = " + (X * Y));
+                Console.WriteLine("X / Y = " + (X / Y));
+                Console.WriteLine("X % Y = " + (X % Y));
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("N + M = " + (N + M));
+                Console.WriteLine("N - M = " + (N - M));
+                Console.WriteLine("N * M = " + (N * M));
+                Console.WriteLine("N / M = " + (N / M));
+                Console.WriteLine("N % M = " + (N % M));
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("X + N = " + (X + N));
+                Console.WriteLine("Y / M = " + (Y / M));
+                Console.WriteLine("Y % M = " + (Y % M));
+                Console.WriteLine("X * 2 = " + (X * 2));
+                Console.WriteLine("Y * 2 = " + (Y * 2));
+                Console.WriteLine("M * 2 = " + (M * 2));
+                Console.WriteLine("N * 2 = " + (N * 2));
+                Console.WriteLine("X + Y + M + N = " + (X + Y + M + N));
+                Console.WriteLine("X * Y * M * N = " + (X * Y * M * N));
+           
+        }
+    }
+}
